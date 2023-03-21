c25bcbcbc163a3657838cde001607279ecce8894
Bui Kha Minh <buikhaminh2003@gmail.com>
2023-03-21 13:15:01 +0700

    Final Lab3

--- src/js/main.js
+++ src/js/main.js
@@ -14,15 +14,15 @@ if (formSearch) {
 }
 // Cart Product
 var itemList = {
-  sp001: { name: 'Sữa Chua Vị Kiwi', price: 21000, photo: 'images/sanpham/kiwi.jpg' },
-  sp002: { name: 'Sữa Chua Vị Xoài', price: 22000, photo: 'images/sanpham/mango.jpg' },
-  sp003: { name: 'Sữa Chua Vị Dưa lưới', price: 23000, photo: 'images/sanpham/cantaloupe.jpg' },
-  sp004: { name: 'Sữa Chua Vị Mâm Xôi', price: 24000, photo: 'images/sanpham/blackberry.jpg' },
-  sp005: { name: 'Sữa Chua Vị Dâu Tây', price: 25000, photo: 'images/sanpham/strawberry.jpg' },
-  sp006: { name: 'Sữa Chua Vị Việt Quất', price: 26000, photo: 'images/sanpham/blueberry.jpg' },
-  sp007: { name: 'Sữa Chua Vị Bưởi', price: 27000, photo: 'images/sanpham/grapes.jpg' },
-  sp008: { name: 'Sữa Chua Vị Táo Xanh', price: 28000, photo: 'images/sanpham/green-apple.jpg' },
-  sp009: { name: 'Sữa Chua Vị Dứa', price: 29000, photo: 'images/sanpham/pineapple.jpg' }
+  sp001: { name: 'Sữa Chua Vị Kiwi', price: 21000, photo: '../asset/images/sanpham/kiwi.jpg' },
+  sp002: { name: 'Sữa Chua Vị Xoài', price: 22000, photo: '../asset/images/sanpham/mango.jpg' },
+  sp003: { name: 'Sữa Chua Vị Dưa lưới', price: 23000, photo: '../asset/images/sanpham/cantaloupe.jpg' },
+  sp004: { name: 'Sữa Chua Vị Mâm Xôi', price: 24000, photo: '../asset/images/sanpham/blackberry.jpg' },
+  sp005: { name: 'Sữa Chua Vị Dâu Tây', price: 25000, photo: '../asset/images/sanpham/strawberry.jpg' },
+  sp006: { name: 'Sữa Chua Vị Việt Quất', price: 26000, photo: '../asset/images/sanpham/blueberry.jpg' },
+  sp007: { name: 'Sữa Chua Vị Bưởi', price: 27000, photo: '../asset/images/sanpham/grapes.jpg' },
+  sp008: { name: 'Sữa Chua Vị Táo Xanh', price: 28000, photo: '../asset/images/sanpham/green-apple.jpg' },
+  sp009: { name: 'Sữa Chua Vị Dứa', price: 29000, photo: '../asset/images/sanpham/pineapple.jpg' }
 };
 const productList = document.querySelectorAll('.product-item');
 if (productList) {
@@ -79,3 +79,71 @@ if (cartBtn) {
     window.location.href = 'donhang.html';
   };
 }
+function showCart() {
+  const cartDetailRow = document.querySelector('#cart-detail tbody');
+  const totalPrice = document.querySelector('#cart-detail .table-foot__total');
+  const discountPrice = document.querySelector('#cart-detail .table-foot__discount');
+  const taxPrice = document.querySelector('#cart-detail .table-foot__tax');
+  const sumPrice = document.querySelector('#cart-detail .table-foot__sum');
+  let orders = JSON.parse(localStorage.getItem('order'));
+  let html = [];
+  let total = 0;
+  if (cartDetailRow) {
+    orders.forEach((order) => {
+      let keys = Object.keys(order);
+      let { name, price, photo } = itemList[keys[0]];
+      let amount = order[keys[0]];
+      total = total + price * amount;
+      let htmlItem = `<tr class="table-details__row" data-id=${keys[0]}>
+      <td class="table-order-img">
+          <img src=${photo} alt="SP" width="100px">
+      </td>
+      <td colspan="2">${name}</td>
+      <td class="table-right">${amount}</td>
+      <td class="table-right">${new Intl.NumberFormat().format(price)} đ</td>
+      <td class="table-right">${new Intl.NumberFormat().format(amount * price)} đ</td>
+      <td class="table-icon" >
+          <div onclick="hanldRemovePro(event)"><i class="bi bi-trash3-fill"></i></div>
+      </td>
+  </tr>`;
+      html.push(htmlItem);
+      cartDetailRow.innerHTML = html.join('');
+    });
+    let discount = conditionDiscount(total);
+    let tax = 0.1 * (total - total * 0.1);
+    let sum = total - discount + tax;
+    totalPrice.innerHTML = `<td colspan="7">Tổng thành tiền (A) = ${new Intl.NumberFormat().format(total)}đ</td>`;
+    discountPrice.innerHTML = `<td colspan="7">Chiết Khấu (B) = 0.1 x A = ${new Intl.NumberFormat().format(
+      discount
+    )}đ</td>`;
+    taxPrice.innerHTML = `<td colspan="7">Thuế (C) = 10% x (A-B) = ${new Intl.NumberFormat().format(tax)}đ</td>`;
+    sumPrice.innerHTML = `<td colspan="7">Tổng đơn hàng = A - B + C = ${new Intl.NumberFormat().format(sum)}đ</td>`;
+  }
+}
+function hanldRemovePro(event) {
+  let productRow = event.currentTarget.closest('.table-details__row');
+  let productRowId = productRow.dataset.id;
+  let orders = JSON.parse(localStorage.getItem('order'));
+  orders.forEach((order, index) => {
+    if (order[productRowId]) {
+      orders.splice(index, 1);
+    }
+  });
+  localStorage.setItem('order', JSON.stringify(orders));
+  showCart();
+}
+function conditionDiscount(number) {
+  let arrDay = [2, 3, 4];
+  let day = new Date().getDay(); //0-6: sunday-saturday
+  let hour = new Date().getHours();
+  if (arrDay.includes(day + 1) && ((hour >= 7 && hour <= 11) || (hour >= 13 && hour <= 17))) {
+    number = number * 0.1;
+    return number;
+  }
+  return 0;
+}
+// The storage event of the Window interface fires when a storage area (localStorage) has been modified in the context of another document.
+window.onstorage = () => {
+  showCart();
+};
+showCart();
